d21a3fa9876f52c44a093d294351643e28a99471
Rohan Dahal <rohandahal@Rohans-MacBook-Air.local>
2026-01-22 09:08:55 -0600

    chore: init spring boot baseline

---
+++ src/main/java/com/rohan/perfguard_demo_java/PerfguardDemoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.rohan.perfguard_demo_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfguardDemoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfguardDemoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohan/perfguard_demo_java/PerfguardDemoJavaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.rohan.perfguard_demo_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfguardDemoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
